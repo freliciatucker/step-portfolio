6e068d61be60bc04ec5482b24d58b4f69d651c49
Frelicia Tucker <58539760+freliciatucker@users.noreply.github.com>
2020-06-30 02:21:43 -0400

    for the two recent commits (#3)
    * update the readme.md file to describe the repo
    
    * updated project ID in pom
    
    * adding necessary files for personal webpage
    
    * change 1st page's button to link and works!
    
    * style change
    
    * fix buttons
    
    * creates Frelicia's webpage
    
    * /data url shows Hello Frelicia!
    
    * resolves Jared's style comments
    
    * deletes extra readme
    
    * deletes console.log call and deletes CSS formatter
    
    * deletes CSS attachment
    
    * runs script.js thru clang-format

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,28 +1,46 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Adds a random fact to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function addRandomFact() {
+  const facts =
+      ['I have a day named after me', 'I have a dog named Romeo', 
+      'My favorite movie is Les Mis', 
+      'The first word I could spell was "cow"'];
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  // Pick a random fact.
+  const fact = facts[Math.floor(Math.random() * facts.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const factsContainer = document.getElementById('fact-container');
+  factsContainer.innerText = fact;
 }
+
+/**
+* Opens picture up to text of the associated paragraph.
+ */
+function openPicture(num){
+      // Each string corresponds to a picture.
+      const captions= [' Welcome to my page! As you look around,' +
+      'I hope you learn a bit more about me and enjoy some seeing' +
+      'some of my favorite moments in life!', 
+      'I was born prematurely because I shared the room with my twin ' +
+      'sister, Fredericka, who was eating all the food. I had to get outta ' +
+      'there ASAP.',
+      'I grew up with 2 beautiful sisters and a mom and a dad.  We didn\'t ' +
+      'always have much, but we had a good life. I didn\'t get to see my dad ' +
+      'often because he was incarcerated, but I knew he loved and cared for ' +
+      'me very much', 
+      'After his release, he helped sparked my interests in both ' +
+      'STEM and sports by introducing to riddles and home science projects. ' +
+      'Tragically he was killed only 2 years after being released, but I ' +
+      'continue to remember his legacy by learning computer science and ' +
+      'staying physically active today.']
+    
+    // Match picture number to string index.
+    const caption = captions[num];
+
+   // Add string to the page.
+   const hiddenPrint =  document.getElementById('img' + num + '-container');
+   hiddenPrint.innerText = caption; 
+ }
