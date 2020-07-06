8375dbc40113cde26cb9610ba49a5369a651f6bc
frelicia tucker <ftucker@princeton.edu>
2020-07-06 13:23:39 +0000

    deletes 3 unused imported classes

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -13,13 +13,10 @@
 // limitations under the License.
 
 package com.google.sps.servlets;
-
+p   
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.SortDirection;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
