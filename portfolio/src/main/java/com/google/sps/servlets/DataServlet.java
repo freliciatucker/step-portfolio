f6b7ac34838e402004ce20fc907072635726a91c
frelicia tucker <ftucker@princeton.edu>
2020-07-06 17:51:06 +0000

    displays comments on home page

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,29 +14,29 @@
 
 package com.google.sps.servlets;
 
-
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.List;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-    private ArrayList<String> list= new ArrayList<String>();
+    private List<String> list= new ArrayList<String>();
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-   
-    list.add("1");
-    list.add("2");
-    list.add("3");
+
     String json = convertToJson();
     response.setContentType("application/json;");
     response.getWriter().println(json);
@@ -65,10 +65,7 @@ public class DataServlet extends HttpServlet {
     response.setContentType("text/html;");
     response.getWriter().println(text);
 
-<<<<<<< HEAD
-    // Redirects back to index url. 
-    // response.sendRedirect("/index.html");
-=======
+
     Entity commentEntity = new Entity("comment");
     commentEntity.setProperty("name", getParameter(request, "text-input", ""));
     commentEntity.setProperty("ice Cream", iceCream);
@@ -76,8 +73,20 @@ public class DataServlet extends HttpServlet {
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(commentEntity);
->>>>>>> master
-  }
+
+    Query query = new Query("comment");
+    PreparedQuery results = datastore.prepare(query);
+
+      String name = (String) commentEntity.getProperty("name");
+      boolean checkIce= (boolean) commentEntity.getProperty("ice Cream");
+      boolean checkPizza = (boolean) commentEntity.getProperty("pizza");
+
+      String entry = list.size() + ". " + "name: " + name + ". ice cream and pizza: " + checkIce + " and " + checkPizza + "\n";
+      list.add(entry);
+
+    // Redirects back to index url. 
+    response.sendRedirect("/index.html");
+}
 
   /**
    * @return the request parameter, or the default value if the parameter
@@ -96,16 +105,18 @@ public class DataServlet extends HttpServlet {
    */
   private String convertToJson() {
     // empty list case
-    if (list.size()==0)
-    return "empty list.";
+    if (list.size() == 0){
+      return "{}";
+    }
 
     StringBuilder toConvert = new StringBuilder();
     toConvert.append("{");
-     for (int i=0; i<list.size()-1; i++){
-         toConvert.append(list.get(i) + ", ");
-    } 
+     for (int i = 0; i < list.size() - 1; i ++){
+         toConvert.append(list.get(i));
+             } 
 
     toConvert.append(list.get(list.size()-1) + "}");
     return toConvert.toString();
   }
+
 }
