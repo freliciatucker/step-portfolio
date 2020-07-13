14664bf5f846074f8abb2f1d2d45c67dadf8eb87
frelicia tucker <ftucker@princeton.edu>
2020-07-13 13:29:17 +0000

    attempts to add map to page, however overflow is hidden

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -151,9 +151,13 @@ function addResponseToDom(form) {
 
 /** Creates a map and adds it to the page. */
 function createMap() {
-    console.log("in create map function");
-    
   const map = new google.maps.Map(
       document.getElementById('map'),
-      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+      {center: {lat: 33.509, lng: -81.72}, zoom: 16});
+      
+  const trexMarker = new google.maps.Marker({
+    position: {lat: 33.509, lng: -81.72},
+    map: map,
+    title: 'My House'
+  });
 }
