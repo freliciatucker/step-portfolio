1ed3bf094b42c6abc87253789c4aff608f692abc
frelicia tucker <ftucker@princeton.edu>
2020-07-14 02:34:38 +0000

    adds necessary pom dependency for junit

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -19,5 +19,7 @@ import java.util.Collection;
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     throw new UnsupportedOperationException("TODO: Implement this method.");
+
+    
   }
 }
